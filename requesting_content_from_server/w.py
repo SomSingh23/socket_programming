b70cef9f10704fbf32239f391ea1bd0fd6cc1bef
som <githubsom@gmail.com>
2023-02-01 19:56:27 +0530

    modified w.py

---
+++ requesting_content_from_server/w.py
@@ -0,0 +1,8 @@
+import os
+files = os.listdir()
+# array = []
+# with open('server1.txt') as obj:
+#     _list = obj.readlines()
+#     for x in _list:
+#         array.append(x.split())
+# print(array)
